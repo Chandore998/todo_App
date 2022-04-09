195e45c1fe4f7ee3e13c251cd37b9b23872ec4c0
Ankit chandore <ankitchandore997@gmail.com>
2022-04-09 15:30:27 +0530

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-
-
-### `To-Do APP`
-
-TAKE A QUICK LOOK OF MY TO-DO APP 
-CLICK HERE:-  https://chandore998.github.io/todo_App/
-
--- src/components/redux/globalStore/GlobalStore.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {createStore} from 'redux'
-import rootReducer from '../reducer/rootReducer'
-
-
-let store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-export default store;
